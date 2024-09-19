d694544ed1615d73bfda5fa2af95fb015c85abd3
Olah Szabolcs <you@example.com>
2024-09-19 11:36:06 +0200

    kuki

--- vegyes_feladatok/obj/Debug/net8.0/vegyes_feladatok.AssemblyInfo.cs
+++ vegyes_feladatok/obj/Debug/net8.0/vegyes_feladatok.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vegyes_feladatok")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c834ef4ef5506f1be55b76ebcb5d08fc70cf7f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ce5d460e3cdf9c82d354589ad914f88c52d669")]
 [assembly: System.Reflection.AssemblyProductAttribute("vegyes_feladatok")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vegyes_feladatok")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
